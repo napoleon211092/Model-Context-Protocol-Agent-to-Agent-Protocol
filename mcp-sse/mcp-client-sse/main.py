88515d30f517c4e9f6622d30c10005c088115ef5
researcher <researcher>
2025-05-18 12:19:33 +0200

    mcp sse

---
+++ mcp-sse/mcp-client-sse/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from mcp-client-sse!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ mcp-sse/mcp-server-sse/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from mcp-server-sse!")
+
+
+if __name__ == "__main__":
+    main()
